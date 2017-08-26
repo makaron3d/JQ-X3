5f21d16a04d03908e4b4631e60432e12b7c11af3
makaron3d <makaron3d@gmail.com>
2017-08-26 20:59:15 +0200

    039

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     var carouselList = $('#carousel ul');
     function changeSlide() {
         carouselList.animate({marginLeft: -480}, 1000, moveFirstSlide); 
-        var moveFirstSlide = (function() {
+        function moveFirstSlide() {
             var firstItem = carouselList.find('li:first');
             var lastItem = carouselList.find('li:last');
             lastItem.after(firstItem);
             carouselList.css({marginLeft: 0});
-        });
+        }
     } setInterval(changeSlide, 3000);
 });
